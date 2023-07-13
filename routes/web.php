ad6f5bf095138709996d67e496744f832e1ec8ae
scamarenas <scamarenas481@gmail.com>
2023-07-12 18:23:35 -0600

    creacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,45 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   //return view('welcome');
+   return "Bienvenido a la pagina principal este sera el login";
+});
+
+Route::get('usuarios',function(){
+    return "bienvenido a la pagina usuarios";
+});
+
+Route::get('conteos',function(){
+    return "bienvenido a la pagina conteos";
+});
+Route::get('productos',function(){
+    return "bienvenido a la pagina productos";
+});
+Route::get('marcas/',function(){
+    return "bienvenido a la pagina Marcas";
+});
+Route::get('fabricantes',function(){
+    return "bienvenido a la pagina fabricantes";
+});
+Route::get('proveedores',function(){
+    return "bienvenido a la pagina usuarios";
+});
+Route::get('ubicaciones',function(){
+    return "bienvenido a la pagina ubicaciones";
+});
+
+Route::get('lineas',function(){
+    return "bienvenido a la pagina lineas";
+});
+Route::get('unidades',function(){
+    return "bienvenido a la pagina unidades";
+});
+Route::get('ordenes',function(){
+    return "bienvenido a la pagina ordenes";
+});
+Route::get('incidencias',function(){
+    return "bienvenido a la pagina incidencias";
+});
+Route::get('faltantes',function(){
+    return "bienvenido a la pagina faltantes";
 });
